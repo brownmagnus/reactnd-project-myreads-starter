6fcc709cfb6757c9e0e9c21dfd97429d125d8a2a
Brownmagnus Olivers <brownmagnuso@gmail.com>
2018-10-21 01:13:19 -0400

    Apply BooksAPi into the homepage

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from '../../BooksAPI'
 
 import Shelf from '../shelf'
 
 class homePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      books: []
+    }
+  }
+  componentDidMount() {
+    BooksAPI.getAll()
+    .then(resp => {
+      console.log(resp);
+      this.setState({ books: resp})
+    });
+  }
+
+
   render() {
     return (
       <div className="list-books">
