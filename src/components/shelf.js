153e245807b679a0043c2721384dc7ee45769d4f
Brownmagnus Olivers <brownmagnuso@gmail.com>
2018-10-21 00:41:21 -0400

    Render book from book component file

---
+++ src/components/shelf.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+
+import Book from './book'
+
+class shelf extends Component {
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Read</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            <li>
+            <Book />
+            </li>
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default shelf
